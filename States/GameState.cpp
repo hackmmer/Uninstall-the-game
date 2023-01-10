1f62ca022eddcde9183dc665670a12dad5a81e4e
Blizz Softword <blizzsoftword@gmail.com>
2023-01-09 20:54:01 -0500

    Implemented the new virtual funcs and added Testing code

--- States/GameState.cpp
+++ States/GameState.cpp
@@ -10,11 +10,13 @@ GameState::~GameState()
 
 void GameState::endState()
 {
+    std::cout << "Ending state...!!" << "\n";
 }
 
 void GameState::update(const float &df)
 {
-    
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        std::cout<<"A\n";
 }
 
 void GameState::render(sf::RenderTarget *target)
