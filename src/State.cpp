bf0753914747f6d70894e9122359caffc961d0b8
Blizz Softword <blizzsoftword@gmail.com>
2023-01-09 20:53:04 -0500

    Added new functions to quit the state and verify if it is need to quit

--- src/State.cpp
+++ src/State.cpp
@@ -3,8 +3,22 @@
 State::State(sf::RenderWindow *window)
 {
     this->window = window;
+    this->quit = false;
 }
 
 State::~State()
 {
+}
+
+void State::checkQuit()
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+    {
+        this->quit = true;
+    }
+}
+
+const bool State::getQuit() const
+{
+    return this->quit;
 }
