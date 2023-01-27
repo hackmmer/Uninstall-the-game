4f450804d2a4fffe580ba259df20f9c6a4f03864
Blizz Softword <blizzsoftword@gmail.com>
2023-01-27 13:47:06 -0500

    Added more Labels to see

--- States/GameState.cpp
+++ States/GameState.cpp
@@ -2,6 +2,10 @@
 eng::Entity player;
 sf::Font f;
 eng::Label lbl("Hola Mundo", 27, 15, 17, f);
+eng::Label lbl2("Start Game", 17, 15, 17*2*1.4, f);
+eng::Label lbl3("Opciones", 17, 15, 17*3*1.4, f);
+eng::Label lbl4("Extra", 17, 15, 17*4*1.4, f);
+eng::Label lbl5("Salir", 17, 15, 17*5*1.4, f);
 
 GameState::GameState(sf::RenderWindow *window) : State(window)
 {
@@ -20,7 +24,15 @@ void GameState::endState()
 void GameState::update(const float &dt)
 {
     lbl.font = f;
+    lbl2.font = f;
+    lbl3.font = f;
+    lbl4.font = f;
+    lbl5.font = f;
     lbl.update(dt);
+    lbl2.update(dt);
+    lbl3.update(dt);
+    lbl4.update(dt);
+    lbl5.update(dt);
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
     {
         player.move(-1, 0, dt);
@@ -45,4 +57,8 @@ void GameState::render(sf::RenderTarget *target)
         target = this->window;
     player.draw(target);
     lbl.draw(target);
+    lbl2.draw(target);
+    lbl3.draw(target);
+    lbl4.draw(target);
+    lbl5.draw(target);
 }
