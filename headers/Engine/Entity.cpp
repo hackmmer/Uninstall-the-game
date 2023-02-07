8c6ed08a15cd76ac326498d4ca1f794a7fc2ece9
Blizz Softword <blizzsoftword@gmail.com>
2023-02-06 21:29:22 -0500

    Updated the Engine stuffs [UN-TESTED THE BUTTON CLASS]

--- headers/Engine/Entity.cpp
+++ headers/Engine/Entity.cpp
@@ -4,7 +4,7 @@ void eng::Entity::move(const float x, const float y, const float &dt)
 {
     this->x = x * speed * dt;
     this->y = y * speed * dt;
-    this->sprite.move(sf::Vector2f(x, y));
+    this->sprite->move(sf::Vector2f(x, y));
 }
 
 void eng::Entity::update(const float &dt)
@@ -16,8 +16,19 @@ void eng::Entity::draw(sf::RenderTarget *target)
 
     if (this->visible)
     {
-        target->draw(this->sprite);
+        target->draw(*this->sprite);
     }
+}
+
+void eng::Entity::loadSprite(sf::Texture *texture)
+{
+    this->EntityTexture = texture;
+    this->sprite = new sf::Sprite(*this->EntityTexture);
+}
+
+void eng::Entity::setPosition(const float x, const float y)
+{
+    this->sprite->setPosition(x, y);
 }
 
 eng::Entity::Entity() : Object("Entity")
