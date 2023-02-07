c67f4042eb73ecdd2d26aa0a7d1828832d8d9839
Blizz Softword <blizzsoftword@gmail.com>
2023-02-06 21:29:22 -0500

    Updated the Engine stuffs [UN-TESTED THE BUTTON CLASS]

--- headers/Engine/Entity.h
+++ headers/Engine/Entity.h
@@ -8,7 +8,8 @@ namespace eng
     class Entity : public Object
     {
     protected:
-        sf::Sprite sprite;
+        sf::Sprite* sprite;
+        sf::Texture* EntityTexture;
         float speed;
 
         float x;
@@ -19,6 +20,9 @@ namespace eng
 
         void update(const float &dt);
         void draw(sf::RenderTarget *target);
+
+        void loadSprite(sf::Texture* texture);
+        void setPosition(const float x, const float y);
 
         Entity();
         Entity(std::string name);
